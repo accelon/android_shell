bd07aa84d0b2b4571b7313382bf410bb87b0cce9
yapcheahshen@gmail.com <yapcheahshen@gmail.com>
2023-01-30 19:02:46 +0800

    reduce apk size

--- build-apk.cmd
+++ build-apk.cmd
@@ -2,4 +2,8 @@ del/q/s app\src\main\assets\
 copy/y ..\accelon22\dist\*.* app\src\main\assets\ 
 @REM copy needed database
 copy/y ..\accelon22\dist\cm\*.* app\src\main\assets\cm
+del app\src\main\assets\accelon22.manifest
+del app\src\main\assets\sw.js
+del app\src\main\assets\index.html
+ren app\src\main\assets\index-android.html index.html
 gradlew assembleDebug
