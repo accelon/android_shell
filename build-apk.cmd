80c4f58867117052d840b560e25b2805a774afe5
yapcheahshen@gmail.com <yapcheahshen@gmail.com>
2023-01-30 19:08:54 +0800

    remove unused file in android

--- build-apk.cmd
+++ build-apk.cmd
@@ -5,5 +5,9 @@ copy/y ..\accelon22\dist\cm\*.* app\src\main\assets\cm
 del app\src\main\assets\accelon22.manifest
 del app\src\main\assets\sw.js
 del app\src\main\assets\index.html
+del app\src\main\assets\index-toolbox.*
+del app\src\main\assets\codemirror.*
+del app\src\main\assets\ambiance.css
+del app\src\main\assets\.args
 ren app\src\main\assets\index-android.html index.html
 gradlew assembleDebug
